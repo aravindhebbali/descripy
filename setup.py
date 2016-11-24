4cca03a0154245385df946d11f1139c43f5428e7
rsquaredin <rsquaredin@gmail.com>
2016-11-24 23:59:08 +0530

    add setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='descripy',
+      version='0.1.0',
+      description='Generate descriptive statistics and explore statistical distributions.',
+      url='https://github.com/rsquaredacademy/descripy',
+      author='Aravind Hebbali',
+      author_email='hebbali.aravind@gmail.com',
+      license='MIT',
+      packages=['descripy'],
+      zip_safe=False)
